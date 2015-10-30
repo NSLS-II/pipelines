d0b2072a97fbf8d956332178bedea7eccb5c4932
Eric Dill <edill@bnl.gov>
2015-10-30 15:43:15 -0400

    Add versioneer and valid setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import versioneer
+
+setuptools.setup(
+    name='pipelines',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Eric Dill',
+    author_email='edill@bnl.gov',
+    description='Automagically generate analysis pipelines',
+    url='http://github.com/NSLS-II/pipelines',
+    platforms='Linux',
+    license="BSD",
+    packages=setuptools.find_packages(),
+    package_data={'nb': ['*.ipynb']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pipeline = pipelines.main:main',
+        ],
+    },
+)
